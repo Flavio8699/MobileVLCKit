2e362f140dae674d15e40bfdc2a8db70b475f257
Flavio Matias <flavio8699@gmail.com>
2024-02-21 20:40:03 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,18 +3,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "VLCKit",
+    name: "MobileVLCKit",
     products: [
-        .library(name: "TVVLCKit", targets: ["TVVLCKit"]),
         .library(name: "MobileVLCKit", targets: ["MobileVLCKit"])
     ],
     targets: [
-        .binaryTarget(
-            name: "TVVLCKit",
-            url: "https://test-vlc.s3.eu-central-1.amazonaws.com/TVVLCKit.xcframework.zip",
-            checksum: "f8e97eb6de2acd56a3ee56982c308e65175d10869fb36b25b34af2a4dda742c7"
-        ),
-        
         .binaryTarget(
             name: "MobileVLCKit",
             url: "https://test-vlc.s3.eu-central-1.amazonaws.com/MobileVLCKit.xcframework.zip",
