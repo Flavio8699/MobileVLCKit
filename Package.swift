d8a5ea6f679bd3c14eff44b2171cd5880a471bdd
Flavio Matias <flavio8699@gmail.com>
2024-02-24 14:41:21 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MobileVLCKit",
-            url: "https://drive.flavio8699.synology.me/d/s/xObVIVNewTlxHhbZcMDsUqrYMoSsX4Wu/u9JX4jAlJIQJAiHvZNXLfffswynX5WPm-I7JgwbWYKAs",
+            url: "https://test-vlc.s3.eu-central-1.amazonaws.com/MobileVLCKit.xcframework.zip",
             checksum: "test"
         ),
     ]
